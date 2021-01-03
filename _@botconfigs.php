8ef51c0c85158554af2d962fe4afa4cacb4f0736
ABHISHEK SENGUPTA <40954336+abhisheksengupt@users.noreply.github.com>
2021-01-03 21:57:25 +0530

    Update _@botconfigs.php

--- _@botconfigs.php
+++ _@botconfigs.php
@@ -2,13 +2,13 @@
 
 //Bot written by Jamron (From FlixTV)
 
-$bot_id = '';
+$bot_id = '1482105344';
 
-$bot_token = '';
+$bot_token = '1482105344:AAHDj4cEb7ni59Agu5j7reWyHZgVfkBkNa8';
 
-$bot_username = '';
+$bot_username = 'bayfilesuploaderprubot';
 
-$sudo_owner = '';
+$sudo_owner = '978649607';
 
 //Do Not Miss slash at starting of command below
 
